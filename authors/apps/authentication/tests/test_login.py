4d06336ff996943cb7843ba29b5b46e90a9085d8
Lwasa Lamech <lamech@andela.com>
2019-04-24 19:56:01 +0300

    Chore (unit tests): Write unit tests for existing features.
    The unit tests make sure that the current features are not failing and also serve a purpose of ensuring that nothing any features added to the codebase in the future don't break the code there by causing these features to fail
    .
    
    [Finished #165273554]

---
+++ authors/apps/authentication/tests/test_login.py
@@ -0,0 +1,66 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+
+class TestLogin(APITestCase):
+    login_url = '/api/users/login/'
+    signup_url = '/api/users/'
+
+    def setUp(self):
+        user_data = {
+            "user": {
+                "username": "janee",
+                "email": "janee@bg.com",
+                "password": "simplepassword"
+            }
+        }
+        
+        response = self.client.post(self.signup_url, data=user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_wrong_email(self):
+        response = self.client.post(
+            self.login_url, 
+            data={'user':{'email':'jane@bg.com', 'password':'simplepassword'}}, 
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get('errors')
+        .get('error')[0], "A user with this email and password was not found.")
+
+    def test_wrong_password(self):
+        response = self.client.post(
+            self.login_url, 
+            data={'user':{'email':'janee@bg.com', 'password':'wrongpassword'}}, 
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get('errors')
+        .get('error')[0], "A user with this email and password was not found.")
+
+    def test_empty_email(self):
+        response = self.client.post(
+            self.login_url, 
+            data={'user':{'email':'', 'password':'simplepassword'}}, 
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get('errors')
+        .get('email')[0], "This field may not be blank.")
+
+    def test_empty_email(self):
+        response = self.client.post(
+            self.login_url, 
+            data={'user':{'email':'janee@bg.com', 'password':''}}, 
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get('errors')
+        .get('password')[0], "This field may not be blank.")
+
+
+    def test_login_success(self):
+        response = self.client.post(
+            self.login_url, 
+            data={'user':{'email':'janee@bg.com', 'password':'simplepassword'}},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
